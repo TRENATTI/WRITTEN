e4b0c558ce7e428665e8033465ec70eab3e91cfd
Cynthia Vlalence <50034413+VLALENCE@users.noreply.github.com>
2025-09-02 23:49:15 -0500

    Update BANNER.js -> Create multi group ban method

--- OPERATORS/BANNER.js
+++ OPERATORS/BANNER.js
@@ -12,54 +12,54 @@ import * as dotenv from "dotenv"
 import * as fs from "fs"
 dotenv.config()
 
-const data = fs.readFileSync('../LISTING/index.json', 'utf8')
+const user_data = fs.readFileSync('../LISTING/index.json', 'utf8')
+const group_data = fs.readFileSync('./groups.json', 'utf8')
 
 const cookie = process.env.RBX_COOKIE
-const groupId = process.env.GROUPID
 
 let xCsrfToken = ""
 
 
 // EXECUTTION
 
-var parsedObject = JSON.parse(data)
-
+var parsedObject = JSON.parse(user_data)
+var GroupIds = JSON.parse(group_data)
  for (const key in parsedObject.users) {
      if (parsedObject.users.hasOwnProperty(key)) { 
         const ids = parsedObject.users[key].associatedAccounts.robloxAccounts = parsedObject.users[key].associatedAccounts.robloxAccounts.split(',')
         for (const UserId in ids) { 
+            for (const GroupId in GroupIds) {
+                var GET_URL = `https://groups.roblox.com/v1/groups/${GroupId}/bans`
+                var POST_URL =`https://groups.roblox.com/v1/groups/${GroupId}/bans/${ids[UserId]}`
+                var DELETE_URL =`https://groups.roblox.com/v1/groups/${GroupId}/bans/${UserId}`
 
-            var GET_URL = `https://groups.roblox.com/v1/groups/${groupId}/bans`
-            var POST_URL =`https://groups.roblox.com/v1/groups/${groupId}/bans/${ids[UserId]}`
-            var DELETE_URL =`https://groups.roblox.com/v1/groups/${groupId}/bans/${UserId}`
-
-            const rbxRequest = async (verb, url, body) => {
-                const response = await fetch(url, {
-                    headers: {
-                        Cookie: `.ROBLOSECURITY=${cookie}`,
-                        "x-csrf-token": xCsrfToken,
-                        "Content-Length": body?.length.toString() || "0"
-                    },
-                    method: "POST",
-                    body: body || ""
-                })
-            
-                if (response.status == 403) {
-                    if (response.headers.has("x-csrf-token")) {
-                        xCsrfToken = response.headers.get("x-csrf-token")
-                        return rbxRequest(verb, url, body)
+                const rbxRequest = async (verb, url, body) => {
+                    const response = await fetch(url, {
+                        headers: {
+                            Cookie: `.ROBLOSECURITY=${cookie}`,
+                            "x-csrf-token": xCsrfToken,
+                            "Content-Length": body?.length.toString() || "0"
+                        },
+                        method: "POST",
+                        body: body || ""
+                    })
+                
+                    if (response.status == 403) {
+                        if (response.headers.has("x-csrf-token")) {
+                            xCsrfToken = response.headers.get("x-csrf-token")
+                            return rbxRequest(verb, url, body)
+                        }
                     }
+                
+                    return response
                 }
-            
-                return response
-            }
-            
-            const response = await rbxRequest("POST", "https://auth.roblox.com/v2/session/refresh")
-            console.log(response.status)
-            
-            const banresponse = await rbxRequest("POST", POST_URL)
-            console.log(response.status)
-            
+                
+                const response = await rbxRequest("POST", "https://auth.roblox.com/v2/session/refresh")
+                console.log(response.status)
+                
+                const banresponse = await rbxRequest("POST", POST_URL)
+                console.log(response.status)
+           }
         }
     }
  }
